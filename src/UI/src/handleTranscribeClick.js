1d9dc47c2c54a0eba176cd1ade53fc48433b4b95
Kamran Kamranifard <kamran.kamranifard@riverty.com>
2026-03-02 15:03:58 +0100

    pushed playing audio in frontend + event

--- src/UI/src/handleTranscribeClick.js
+++ src/UI/src/handleTranscribeClick.js
@@ -1,13 +1,26 @@
 import { apiCall } from "./services/api";
+import audioFile from './assets/voice_3.ogg';
 
 export const attachTranscribeClickListener = () => {
     const transcribeBtn = document.querySelector('button');
     if (transcribeBtn) {
-        transcribeBtn.addEventListener('click', () => {
+        transcribeBtn.addEventListener('click', async () => {
             console.log("clicked on button!");
             apiCall();
+            playAudio();
         });
     } else {
-        console.error("### button element not found!")
+        console.error("### button element not found!");
     }
+}
+
+const playAudio = () => {
+    const audioSource = new Audio(audioFile);
+    audioSource.addEventListener('error', (e) => {
+        console.error('Audio error:', e); // check console for 404
+    });
+    audioSource.addEventListener('canplaythrough' , () => {
+        audioSource.play();
+    })
+    console.log(`audio loaded ${audioSource}. Playing... ${audioSource.volume}`);
 }
